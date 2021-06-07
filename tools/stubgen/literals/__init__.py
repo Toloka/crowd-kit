c70ae6bc77ea8ee4945b02dbb897e782f49bdb0b
Stepan Nosov <nosov.stefan98@yandex.ru>
2021-06-07 22:41:56 +0300

    markdown generator
    ref:5a28790e0e68d64fd05e4756b61b030b11c19c08

--- tools/stubgen/literals/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Module contains classes representing values"""
-
-__all__ = ['ReferenceLiteral', 'TypeHintLiteral', 'TypeVarLiteral', 'ValueLiteral']
-
-from .reference_literal import ReferenceLiteral
-from .type_hint_literal import TypeHintLiteral
-from .type_var_literal import TypeVarLiteral
-from .value_literal import ValueLiteral
