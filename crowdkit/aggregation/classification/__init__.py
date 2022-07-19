aa4d7eccc7318eb7c8043bb2ada13ba6a06b890f
pilot7747 <pilot7747@gmail.com>
2022-07-19 12:38:53 +0300

    Add MACE aggregation model

--- crowdkit/aggregation/classification/__init__.py
+++ crowdkit/aggregation/classification/__init__.py
@@ -1,10 +1,11 @@
 __all__ = [
     'DawidSkene',
-    'OneCoinDawidSkene',
     'GLAD',
     'GoldMajorityVote',
-    'MMSR',
+    'MACE',
     'MajorityVote',
+    'MMSR',
+    'OneCoinDawidSkene',
     'Wawa',
     'ZeroBasedSkill'
 ]
@@ -12,7 +13,8 @@ __all__ = [
 from .dawid_skene import DawidSkene, OneCoinDawidSkene
 from .glad import GLAD
 from .gold_majority_vote import GoldMajorityVote
-from .m_msr import MMSR
+from .mace import MACE
 from .majority_vote import MajorityVote
+from .m_msr import MMSR
 from .wawa import Wawa
 from .zero_based_skill import ZeroBasedSkill
