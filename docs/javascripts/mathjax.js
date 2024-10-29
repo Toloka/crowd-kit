c347deb6a26ced67d8596fe153b005d4754cc790
taharallouche <tahar.allouche.pro@gmail.com>
2024-10-29 07:11:59 +0100

    fix: load boldsymbol mathjax extension

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -8,7 +8,9 @@ window.MathJax = {
   options: {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
-  }
+  },
+  loader: { load: ['[tex]/boldsymbol'] },
+  tex: { packages: { '[+]': ['boldsymbol'] } }
 };
 
 document$.subscribe(() => {
